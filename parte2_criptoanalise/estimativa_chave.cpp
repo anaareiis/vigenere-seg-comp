38e36e751d3a5f081a980fc1fde4216ad6504c81
PedroDP1804 <pedropaulacampos@gmail.com>
2026-08-31 11:16:22 -0300

    feat: estimativa do tamanho da chave (finalizado)

--- parte2_criptoanalise/estimativa_chave.cpp
+++ parte2_criptoanalise/estimativa_chave.cpp
@@ -1,12 +1,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
-
-// Copiei pq não tava conseguindo colocar o include, se conseguir pfv troca pra usar o que já tem
-bool is_upper_ascii(const unsigned char value) {return value >= static_cast<unsigned char>('A') && value <= static_cast<unsigned char>('Z');}
-bool is_lower_ascii(const unsigned char value){return value >= static_cast<unsigned char>('a') && value <= static_cast<unsigned char>('z');}
 
 /**
  * @brief Estima o tamanho da chave de um texto cifrado com a Cifra de Vigenère, utilizando o Método do Índice de Coincidência.
@@ -26,13 +24,10 @@ vector<int> estima_tamanho_chave(
     for (int i = 0; i < tamanho_cyphertext; i++) {
         char caracter = CYPHERTEXT[i];
         
-        if (!is_lower_ascii(caracter) && !is_upper_ascii(caracter))
-        continue;
+        if (!isalpha(caracter))
+            continue;
         
-        if (is_lower_ascii(caracter))
-        cyphertext_limpo += caracter - 32;
-        else
-        cyphertext_limpo += caracter;
+        cyphertext_limpo += toupper(caracter);
     }
     int tamanho_cyphertext_limpo = cyphertext_limpo.length();
 
@@ -44,7 +39,8 @@ vector<int> estima_tamanho_chave(
     // Itera pelos tamanhos possívieis, até o tamanho máximo
     for (int tamanho_testado = 1; tamanho_testado <= MAX_TAMANHO_CHAVE; tamanho_testado++) {
 
-        // Divide o cyphertext em grupos, 
+        // Separa o cyphertext em grupos, em que caracteres adjacentes num grupo são separados no cyphertext pelo tamanho da chave
+        // Ex: Se o tamanho da chave for 3, o grupo 0 vai conter os caracteres 0, 3, 6... do cyphertext; o grupo 1 os caracteres 1, 4, 7...
         vector<string> grupos;
         for (int index_grupo = 0; index_grupo < tamanho_testado; index_grupo++) {
 
@@ -60,9 +56,13 @@ vector<int> estima_tamanho_chave(
         }
 
         // Cálculo do IoC médio para o tamanho testado
-        double ioc_medio = 0.0;        
-        for (string grupo : grupos) {
+        double ioc_medio = 0.0;
+        int grupos_validos = 0;
+        for (const string& grupo : grupos) {
             
+            if (grupo.length() <= 1)
+                continue;
+
             // Calcula a frequência de cada letra
             vector<int> frequencias(26, 0);
             for (char c : grupo) {
@@ -77,25 +77,37 @@ vector<int> estima_tamanho_chave(
             }
             ioc /= grupo.length() * (grupo.length() - 1);
             ioc_medio += ioc;
+
+            grupos_validos++;
             
         }
         
-        // Atualiza a estimativa, se o IoC médio for maior que o da estimativa anterior
-        ioc_medio = ioc_medio / grupos.size();
-        if (ioc_medio > maior_ioc_medio) {
-            maior_ioc_medio = ioc_medio;
-            estimativa_tamanho = tamanho_testado;
-        }
+        // Adiciona o ioc médio e o tamanho testado ao vetor de estimativas
+        if (grupos_validos > 0)  
+            ioc_medio = ioc_medio / grupos_validos;
+        else
+            ioc_medio = 0.0;
+
+        ioc_tamanho.push_back(pair(ioc_medio, tamanho_testado));
 
     }
 
-    return estimativa_tamanho;
+    // Ordenação decrescente pelo ioc
+    sort(ioc_tamanho.begin(), ioc_tamanho.end(), [](pair<double,int> a, pair<double,int> b) {return a.first > b.first;} );
+
+    // Novo vetor só com os tamanhos
+    vector<int> estimativas_ordenadas;
+    for (const auto& p : ioc_tamanho) {
+        estimativas_ordenadas.push_back(p.second);
+    }
+
+    return estimativas_ordenadas;
 }
 
-// Teste
+// Teste simples
 int main() {
 
-    int resultado_teste = estima_tamanho_chave(
+    vector<int> resultado_teste = estima_tamanho_chave(
         (
             // Tamanho da chave: 6
             "Dlb X fiuk mmizczv ih mql pdp onc bwbg ozaityge qsxo qw ltgcwekxgu rcmplitv icw dtrqhbbi gixg kcj jdkb cel X pwnc oxos afc p vcogttms ctkdnbv fn ias upaixa, cel tqdqlvs mvg rkinon kmpvvkeoh ht vym vksck mmizqimg ht vym ikivy, bwx acjbtk-pwztsxf qw pjfop yieiwpvah. Gc qem gxxgtbh, wwucqzxg, qi ikhwfj xaxoulzt bhuvtu, userchx wv za eescjcgx, pwk jtvowjm iacuv ewh rq ewi dbqn pdp hq gcglig gtttgwim gthkfvpeza vvrhipkmg vcpjmfnsptmh mvck igx szkztfsnp xpbbhlt. Chf cxixg wu kptks cegdgs yyw ahjgj wg iitjctl ct umhbfgj bd hpvrqc ioke wu bhuvtu, userchx wv za etwp, sci userchx cetihbcprtar qkikjfgvrvrxg qtkjk wp npxvv vfqa tbf gixg qce xghqwim wba ufut zfgrb eescjcgx. Hq kizx o viqkbon vfpfdnv, ewbqj fn jl sxvz jgrgibpdsu ciqhfkfch ivajqrtz gomgvwuv, mmvsrk bd hpvrqc lcov isoopkivx ttfu xm? Pwk ewh vcj icr fkxpi mc hzvs yowcb lbhj r upg kjf kwhcuva ih spawn t dnvihnfg kppm vcj vd tbpfgxgu efvhxewvvrxg, qi wcx kjf ikhwfj i etwp kppm dtfljvsu ew gxgwcbpgh rcmplitv? Wc mvg fbwxf jrvs, ps fvvdnbev exmv tzowmsqla xgrkxvpmwqe icw rkjtxds ovv lac cim hh pgxcxesf rvs wsofzpewbvl qr hjv kwtfoj wu izgrajks qw bwx aqdmcm, gq stxgrgu jn wsuzzt, mvck bwxm ervchh hfztlsg"
@@ -103,7 +115,10 @@ int main() {
         10
     );
 
-    cout << "O tamanho estimado eh " << resultado_teste << '\n';
+    for (int tam : resultado_teste) {
+        cout << tam << " > ";
+    }
+    cout << '\n';
 
     return 0;
 }
